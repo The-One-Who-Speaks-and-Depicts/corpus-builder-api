f1aa853481b85c78e7e9c07f6e7e4a22b033cfd0
Ilya Afanasev <szrnamerg@gmail.com>
2020-04-18 21:28:40 +0400

    Text output first commit

--- AddFeature.cshtml.cs
+++ AddFeature.cshtml.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CorpusDraftCSharp;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace CroatianProject.Pages.Admin
 {
@@ -15,6 +17,7 @@ namespace CroatianProject.Pages.Admin
         public List<string> textList { get; set; }
         [BindProperty]
         public string textName { get; set; }
+        public List<string> textByWords = new List<string>();
 
         public List<string> getTexts()
         {
@@ -30,6 +33,13 @@ namespace CroatianProject.Pages.Admin
             return existingTexts;
         }
 
+        public DirectoryInfo SearchForText(string textName, string directory)
+        {
+            DirectoryInfo dirTexts = new DirectoryInfo(directory);
+            var searchedDirectory = dirTexts.GetDirectories().Where((dir) => dir.Name == textName).First();
+            return searchedDirectory;
+        }
+
         private IHostingEnvironment _environment;
         public AddFeatureModel(IHostingEnvironment environment)
         {
@@ -43,9 +53,41 @@ namespace CroatianProject.Pages.Admin
                 Redirect("./Error");
             }
         }
-        public IActionResult OnGetShow()
+        public void OnPostShow()
         {
-            return RedirectToPage();
+            var directory = Path.Combine(_environment.ContentRootPath, "database", "texts");
+            List<DirectoryInfo> searchedTexts = new List<DirectoryInfo>();
+            if (textName != "Any")
+            {
+                searchedTexts.Add(SearchForText(textName, directory));            
+            List<Realization> acquiredForms = new List<Realization>();
+            foreach (var text in searchedTexts)
+            {
+                DirectoryInfo dirWords = new DirectoryInfo(Path.Combine(directory, text.Name, "paragraphs"));
+                var dirResults = dirWords.GetDirectories();
+                foreach (var wordDirectory in dirResults)
+                {
+                    var words = wordDirectory.GetFiles();
+                    string s;
+                    foreach (var word in words)
+                    {
+
+                        using (var f = new StreamReader(word.FullName))
+                        {
+                            while ((s = f.ReadLine()) != null)
+                            {
+                                acquiredForms.Add(JsonConvert.DeserializeObject<Realization>(s));
+                            }
+                        }
+                    }
+                }
+            }
+            foreach (var foundWord in acquiredForms)
+            {
+                textByWords.Add("<span onclick=\"textShow();\"> " + foundWord.lexeme + "</span>");
+            }
+            }
+            textList = getTexts();
         }
     }
 }
