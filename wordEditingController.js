3655cb989c464ac835a50d9ff3965e93bd7abd6f
Ilya Afanasev <szrnamerg@gmail.com>
2020-04-18 20:33:05 +0400

    JS onclick resolved

--- wordEditingController.js
+++ wordEditingController.js
@@ -1,12 +1,7 @@
-﻿window.onload = function () {
-
-    //получаем идентификатор элемента
-    var a = document.getElementById('text');
-
-    //вешаем на него событие
-    a.onclick = function () {
-        console.log("Initialized!");
-        //предотвращаем переход по ссылке href
+﻿window.onload = function () {    
+    textShow = function () {
+        console.log("To be implemented!");
         return false;
     }
 }
+
