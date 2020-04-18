8fe8e558a71e50cfe2fa6cb9270edb2e0a3bd43d
Ilya Afanasev <szrnamerg@gmail.com>
2020-04-18 15:09:46 +0400

    start of onclick reaction

--- AddText.cshtml.cs
+++ AddText.cshtml.cs
@@ -252,6 +252,7 @@ namespace CroatianProject.Pages
         }
 
 
+
     }
 }
 
---
+++ wordEditingController.js
@@ -0,0 +1,14 @@
+﻿window.onload = function () {
+
+    //получаем идентификатор элемента
+    var a = document.getElementById('word');
+
+    //вешаем на него событие
+    a.onclick = function () {
+        //производим какие-то действия
+        if (this.innerHTML == 'On') this.innerHTML = 'Off';
+        else this.innerHTML = 'On';
+        //предотвращаем переход по ссылке href
+        return false;
+    }
+}
