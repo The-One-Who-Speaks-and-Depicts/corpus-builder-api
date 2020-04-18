2705ab19abf31ba09f087ba5347bd70f58fbb50c
Ilya Afanasev <szrnamerg@gmail.com>
2020-04-18 20:13:16 +0400

    Feature addition initial commit

---
+++ AddFeature.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CroatianProject.Pages.Admin
+{
+    public class AddFeatureModel : PageModel
+    {
+        [BindProperty]
+        public List<string> textList { get; set; }
+        [BindProperty]
+        public string textName { get; set; }
+
+        public List<string> getTexts()
+        {
+            var directory = Path.Combine(_environment.ContentRootPath, "database", "texts");
+            DirectoryInfo textsDirectory = new DirectoryInfo(directory);
+            var texts = textsDirectory.GetDirectories();
+            List<string> existingTexts = new List<string>();
+            existingTexts.Add("Any");
+            foreach (var text in texts)
+            {
+                existingTexts.Add(text.Name);
+            }
+            return existingTexts;
+        }
+
+        private IHostingEnvironment _environment;
+        public AddFeatureModel(IHostingEnvironment environment)
+        {
+            _environment = environment;
+            try
+            {
+                textList = getTexts();
+            }
+            catch
+            {
+                Redirect("./Error");
+            }
+        }
+        public IActionResult OnGetShow()
+        {
+            return RedirectToPage();
+        }
+    }
+}
