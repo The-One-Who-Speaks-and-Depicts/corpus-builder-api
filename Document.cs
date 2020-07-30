ac50d24da4e5d4166192531f5ebf6c337f0db9ca
Ilia Afanasev <szrnamerg@gmail.com>
2020-07-30 18:04:34 +0400

    Document structure implemented

---
+++ Document.cs
@@ -0,0 +1,52 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace CorpusDraftCSharp
+{
+    public class Document
+    {
+
+
+        #region objectValues
+        [JsonProperty]
+        public string documentID;
+        [JsonProperty]
+        public readonly string documentName;
+        [JsonProperty]
+        public string filePath;
+        [JsonProperty]
+        public string googleDocPath;
+        [JsonProperty]
+        public List<Text> texts = new List<Text>();
+        #endregion
+
+        #region Constructors
+
+        [JsonConstructor]
+        public Document(string _documentID, string _documentName, string _filePath, string _googleDocPath)
+        {
+            documentID = _documentID;
+            documentName = _documentName;
+            filePath = _filePath;
+            googleDocPath = _googleDocPath;
+        }
+
+        #endregion
+        #region PublicMethods
+        public string Jsonize()
+        {
+            string json = JsonConvert.SerializeObject(this);
+            return json;
+        }
+        #endregion
+
+
+
+
+
+    }
+}
