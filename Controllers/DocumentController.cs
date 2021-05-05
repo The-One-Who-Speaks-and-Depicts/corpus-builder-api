751cf2e6de99b5ef9cc469cc3bb488e1e029a562
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-05-05 18:27:40 +0400

    ADD: reference to manuscriptsprocessor

--- Controllers/DocumentController.cs
+++ Controllers/DocumentController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ManuscriptsProcessor.Fields;
+using ManuscriptsProcessor.Values;
+using ManuscriptsProcessor.Units;
 
 namespace corpus_builder_api.Controllers
 {
