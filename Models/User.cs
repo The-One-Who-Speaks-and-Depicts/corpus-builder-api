121d36cbdfc9b2c69722838bcf92e527eebed85d
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-05-09 21:53:19 +0400

    ADD: User model

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+namespace corpus_builder_api.Models
+{
+	class User
+	{
+		private string id;
+		private string password;
+
+		User(string _id, string _password)
+		{
+			id = _id;
+			password = _password;
+		}
+
+		public bool CheckPassword(string _password)
+		{
+			return this.password == _password;
+		}
+	}
+}
