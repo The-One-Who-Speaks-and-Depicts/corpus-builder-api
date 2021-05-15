f3169395afb1d5a8988f7b37688fb2252d2cd1fe
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-05-15 18:42:39 +0400

    ADD: POST query for Field

--- Controllers/FieldController.cs
+++ Controllers/FieldController.cs
@@ -116,6 +116,55 @@ namespace corpus_builder_api.Controllers
 
         // TODO: Change from here; add connections
 
+        [HttpPost]
+        public string Post(string name, string description, string multiplied, string restricted, string host, string possessed, string values)
+        {
+            // check for existing field!
+            if (String.IsNullOrEmpty(name) || String.IsNullOrEmpty(description) || String.IsNullOrEmpty(multiplied) || String.IsNullOrEmpty(restricted) || String.IsNullOrEmpty(host) || String.IsNullOrEmpty(possessed))
+            {
+                return "Error: one of the required fields is empty";
+            }
+            if (restricted == "restricted" && String.IsNullOrEmpty(values))
+            {
+                return "Error: no values for user-restricted field";
+            }
+            IDocumentStore store = new DocumentStore()
+            {
+                Urls = new[] { "http://localhost:8080", },
+            }.Initialize();
+            RavenHelper.EnsureDatabaseExists(store);
+
+            using (store) 
+            {
+                SessionOptions options = new SessionOptions {Database = "Manuscripts", TransactionMode = TransactionMode.ClusterWide};
+                using (IDocumentSession Session = store.OpenSession(options))
+                {
+                    Field addedField = new Field();
+                    addedField.Id = name;
+                    addedField.description = description;
+                    addedField.isMultiple = multiplied == "multiplied" ? true : false;
+                    addedField.isUserFilled = restricted == "restricted" ? false : true;
+                    addedField.host = host;
+                    addedField.possessed = possessed;
+                    if (!addedField.isUserFilled)
+                    {
+                        List<object> newValues = new List<object>(); 
+                        var addedValues = values.Split("<br />").ToList();
+                        for (int i = 0; i < addedValues.Count; i++){
+                            if (!String.IsNullOrEmpty(addedValues[i]))
+                            {
+                                newValues.Add(addedValues[i].Trim());
+                            }
+                        }
+                        addedField.values = newValues;
+                    }
+                    Session.Store(addedField);
+                    Session.SaveChanges();
+                    return "Success";
+                }
+            }
+        }
+
         [HttpPatch]
         public void Change(string id, string filePath, string googleDocPath, string fields)
         {
@@ -170,79 +219,6 @@ namespace corpus_builder_api.Controllers
                 }
             }
         }
-
-        [HttpPost]
-        public void Post(string id, string filePath, string googleDocPath, string name, string fields)
-        {
-            IDocumentStore store = new DocumentStore()
-            {
-                Urls = new[] { "http://localhost:8080", },
-            }.Initialize();
-            RavenHelper.EnsureDatabaseExists(store);
-
-            using (store) 
-            {
-                SessionOptions options = new SessionOptions {Database = "Manuscripts", TransactionMode = TransactionMode.ClusterWide};
-                using (IDocumentSession Session = store.OpenSession(options))
-                {
-                    Manuscript addedManuscript = new Manuscript();
-                    if (!String.IsNullOrEmpty(filePath))
-                    {
-                        addedManuscript.filePath = filePath;
-                    }
-                    if (!String.IsNullOrEmpty(googleDocPath))
-                    {
-                        addedManuscript.googleDocPath = googleDocPath;
-                    }
-                    if (!String.IsNullOrEmpty(fields))
-                    {
-                        List<Dictionary<string, List<Value>>> manuscriptFields = new List<Dictionary<string, List<Value>>>();
-                        List<string> splitFields = fields.Split(';').ToList();
-                        Dictionary<string, List<Value>> fieldsToAdd = new Dictionary<string, List<Value>>();
-                        for (int i = 0; i < splitFields.Count; i++)
-                        {
-                            if (splitFields[i] != "")
-                            {
-                                List<string> fieldAndValues = splitFields[i].Split(":").ToList();
-                                string field = fieldAndValues[0];
-                                List<string> stringValues = fieldAndValues[1].Split("|").ToList();
-                                List<Value> values = new List<Value>();
-                                for (int j = 0; j < stringValues.Count; j++)
-                                {
-                                    if (stringValues[j] != "")
-                                    {
-                                        values.Add(new Value {name = stringValues[j], connectedUnits = new List<Unit>()});
-                                    }
-                                }
-                                fieldsToAdd[field] = values;
-
-                            }
-                        }
-                        manuscriptFields.Add(fieldsToAdd);
-                        addedManuscript.tagging = manuscriptFields;
-                    }
-                    if (!String.IsNullOrEmpty(name) && !String.IsNullOrEmpty(id))
-                    {
-                        addedManuscript.name = name;
-                        Session.Store(addedManuscript, id);
-                    }
-                    if (!String.IsNullOrEmpty(name) && String.IsNullOrEmpty(id))
-                    {
-                        addedManuscript.name = name;
-                        Session.Store(addedManuscript, name);
-                    }
-                    if (String.IsNullOrEmpty(name) && !String.IsNullOrEmpty(id))
-                    {
-                        addedManuscript.name = id;
-                        Session.Store(addedManuscript, id);
-                    }
-                    if (String.IsNullOrEmpty(name) && String.IsNullOrEmpty(id))
-                    {
-                        Session.Store(addedManuscript);
-                    }
-                    Session.SaveChanges();
-                }
-            }
-        }
+        
     }
 }
