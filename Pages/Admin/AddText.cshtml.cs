5300705859f2b43cbab261b24661dc3c5d7821ea
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2022-06-19 00:46:45 +0400

    fix: python deleted

--- Pages/Admin/AddText.cshtml.cs
+++ Pages/Admin/AddText.cshtml.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using IronPython.Hosting;
-using LiteDB;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Scripting.Hosting;
 using CorpusDraftCSharp;
 using Newtonsoft.Json;
 
@@ -27,14 +23,6 @@ namespace CroatianProject.Pages
         public AddTextModel(IWebHostEnvironment environment)
         {
             _environment = environment;
-            try
-            {
-                FieldList = getFields();
-            }
-            catch
-            {
-                Redirect("./Error");
-            }
         }
 
 
@@ -45,29 +33,32 @@ namespace CroatianProject.Pages
         [BindProperty]
         public string documentPicked { get; set; }
         [BindProperty]
-        public List<Document> documents
+        public List<string> documents
         {
             get
             {
-                List<Document> deserializedDocuments = new List<Document>();
-                try
-                {
-                    DirectoryInfo docDirectory = new DirectoryInfo(Path.Combine(_environment.ContentRootPath, "database", "documents"));
-                    var jsonedDocuments = docDirectory.GetFiles();
-                    foreach (var doc in jsonedDocuments)
-                    {
-                        using (StreamReader r = new StreamReader(doc.FullName))
-                        {
-                            deserializedDocuments.Add(JsonConvert.DeserializeObject<Document>(r.ReadToEnd()));
-                        }
-                    }
-                    return deserializedDocuments;
-                }
-                catch
+                var deserializedDocuments = new List<string>();
+                var pathToDocuments = Path.Combine(_environment.ContentRootPath, "database", "documents");
+                Directory.CreateDirectory(pathToDocuments);
+                var docDirectory = new DirectoryInfo(pathToDocuments);
+                var jsonedDocuments = docDirectory.GetFiles();
+                if (jsonedDocuments.Length < 1)
                 {
                     return deserializedDocuments;
                 }
+                return jsonedDocuments
+                /* Select(x => (x) => {
 
+                }). */
+                .Select(file => file.FullName)
+                .Select(name => {
+                    using (StreamReader r = new StreamReader(name))
+                    {
+                        return JsonConvert.DeserializeObject<Document>(r.ReadToEnd());
+                    }
+                })
+                .Select(document => document.documentName + "[" + document.documentID + "]")
+                .ToList();
             }
         }
         [BindProperty]
@@ -81,118 +72,125 @@ namespace CroatianProject.Pages
         [BindProperty]
         public string connections { get; set; }
         [BindProperty]
-        public List<string> FieldList { get; set; }
-
-        public List<string> getFields()
+        public List<string> FieldList
         {
-            List<string> existingFields = new List<string>();
-            try
+            get
             {
-                var directory = Path.Combine(_environment.ContentRootPath, "wwwroot", "database", "fields");
-                DirectoryInfo fieldsDirectory = new DirectoryInfo(directory);
+                List<string> existingFields = new List<string>();
+                var pathToFields = Path.Combine(_environment.ContentRootPath, "wwwroot", "database", "fields");
+                Directory.CreateDirectory(Path.Combine(pathToFields));
+                DirectoryInfo fieldsDirectory = new DirectoryInfo(pathToFields);
                 var fields = fieldsDirectory.GetFiles();
                 existingFields.Add("Any");
+                Console.WriteLine(fields.Length.ToString());
+                if (fields.Length < 1)
+                {
+                    return existingFields;
+                }
                 foreach (var field in fields)
                 {
                     existingFields.Add(field.Name.Split(".json")[0]);
                 }
+                return existingFields;
             }
-            catch
-            {
-
-            }
-            return existingFields;
         }
 
-        [HttpGet]
         public void OnGet(string documentPicked)
         {
-            try
+            if (!String.IsNullOrEmpty(documentPicked) && !String.IsNullOrWhiteSpace(documentPicked))
             {
-                var documentID = documentPicked.Split('_')[0];
-                foreach (var doc in documents)
+                var documentID = documentPicked.Split('[')[1].Split(']')[0];
+                var files = new DirectoryInfo(Path.Combine(_environment.ContentRootPath, "database", "documents")).GetFiles();
+                var requiredDocFile = files
+                .Where(x => x.Name.Split('_')[0] == documentID)
+                .Single();
+                using (var r = new StreamReader(requiredDocFile.FullName))
                 {
-                    if (doc.documentID == documentID)
-                    {
-                        googleDocPath = doc.googleDocPath;
-                        analyzedDocument = doc;
-                    }
+                    var requiredDoc = JsonConvert.DeserializeObject<Document>(r.ReadToEnd());
+                    googleDocPath = requiredDoc.googleDocPath;
+                    analyzedDocument = requiredDoc;
                 }
-            }
-            catch
-            {
-
             }
         }
 
 
         public IActionResult OnPostProcess()
         {
-            try
+
+            Text addedText = new Text(analyzedDocument, analyzedDocument.texts.Count.ToString(), textName);
+            addedText.textMetaData = new List<Dictionary<string, List<Value>>>();
+            addedText.textMetaData.Add(new Dictionary<string, List<Value>>());
+            if (!String.IsNullOrEmpty(connections) && !String.IsNullOrWhiteSpace(connections))
             {
-                ScriptEngine engine = Python.CreateEngine();
-                ScriptScope scope = engine.CreateScope();
-                var paths = engine.GetSearchPaths();
-                engine.SetSearchPaths(paths);
-                var pythonFilePath = Path.Combine(_environment.ContentRootPath, "Scripts", "analysis.py");
-                engine.ExecuteFile(pythonFilePath, scope);
-                dynamic function = scope.GetVariable("analysis");
-                IList<object> result = function(processedString, stopSymbols, decapitalization.ToString());
-                Text addedText = new Text(analyzedDocument, analyzedDocument.texts.Count.ToString(), textName);
-                addedText.textMetaData = new List<Dictionary<string, List<Value>>>();
-                addedText.textMetaData.Add(new Dictionary<string, List<Value>>());
-                if (!String.IsNullOrEmpty(connections) && !String.IsNullOrWhiteSpace(connections))
+                string[] tags = connections.Split("\n");
+                foreach (string tag in tags)
                 {
-                    string[] tags = connections.Split("\n");
-                    foreach (string tag in tags)
+                    if (!String.IsNullOrWhiteSpace(tag) && !String.IsNullOrEmpty(tag))
                     {
-                        if (!String.IsNullOrWhiteSpace(tag) && !String.IsNullOrEmpty(tag))
+                        string key = tag.Split("=>")[0];
+                        string[] stringValues = tag.Split("=>")[1].Split(';');
+                        List<Value> typedValues = new List<Value>();
+                        foreach (string stringValue in stringValues)
                         {
-                            string key = tag.Split("=>")[0];
-                            string[] stringValues = tag.Split("=>")[1].Split(';');
-                            List<Value> typedValues = new List<Value>();
-                            foreach (string stringValue in stringValues)
+                            if (!String.IsNullOrEmpty(stringValue) && !String.IsNullOrWhiteSpace(stringValue))
                             {
-                                if (!String.IsNullOrEmpty(stringValue) && !String.IsNullOrWhiteSpace(stringValue))
-                                {
-                                    typedValues.Add(new Value(stringValue));
-                                }
+                                typedValues.Add(new Value(stringValue));
                             }
-                            addedText.textMetaData[0].Add(key, typedValues);
                         }
+                        addedText.textMetaData[0].Add(key, typedValues);
                     }
-                }
-                for (int i = 0; i < result.Count; i++)
-                {
-                    var clauseFullData = (IList<object>)result[i];
-                    Clause addedClause = new Clause(addedText, i.ToString(), (string)clauseFullData[0]);
-                    var realizations = (IList<object>) clauseFullData[1];
-                    for (int j = 0; j < realizations.Count; j++)
-                    {
-                        var realizationFullData = (IList<object>) realizations[j];
-                        Realization addedRealization = new Realization(addedClause, j.ToString(), (string)realizationFullData[0], (string)realizationFullData[1]);
-                        var graphemes = (IList<object>) realizationFullData[2];
-                        for (int k = 0; k < graphemes.Count; k++)
-                        {
-                            addedRealization.letters.Add(new Grapheme(addedRealization, k.ToString(), (string) graphemes[k]));
-                        }
-                        addedClause.realizations.Add(addedRealization);
-                    }
-                    addedText.clauses.Add(addedClause);
-                }
-                analyzedDocument.texts.Add(addedText);
-                using (StreamWriter w = new StreamWriter(Path.Combine(_environment.ContentRootPath, "database", "documents", analyzedDocument.documentID + "_" + analyzedDocument.documentName + ".json")))
-                {
-                    w.Write(analyzedDocument.Jsonize());
                 }
             }
-            catch (Exception e)
+            var clauses = processedString.Split(new char[] {'\n', '\r'}).Where(x => x != "").ToList();
+            var wordsByClauses = new List<DecomposedClause>();
+            foreach (var clause in clauses)
             {
-                FileStream fs = new FileStream("result1.txt", FileMode.Create);
-                using (StreamWriter w = new StreamWriter(fs))
+                var tokens = clause.Split(' ').Where(x => x != "").ToList();
+                var preparedTokens = new List<DecomposedToken>();
+                foreach (var token in tokens)
                 {
-                    w.Write(e.ToString());
+                    var lexemeTwo = token;
+                    if (!String.IsNullOrEmpty(stopSymbols) && !String.IsNullOrWhiteSpace(stopSymbols))
+                    {
+                        lexemeTwo = String.Join("", token.ToCharArray().Where(x => !stopSymbols.Contains(x)).ToList());
+                    }
+                    if (decapitalization)
+                    {
+                        lexemeTwo = lexemeTwo.ToLower();
+                    }
+                    var graphemes = token.ToCharArray().Select(x => x.ToString()).ToList();
+                    preparedTokens.Add(new DecomposedToken
+                    {
+                        lexemeOne = token,
+                        lexemeTwo = lexemeTwo,
+                        graphemes = graphemes
+                    });
                 }
+                wordsByClauses.Add(new DecomposedClause
+                {
+                    clause = clause,
+                    tokens = preparedTokens
+                });
+            }
+            for (int i = 0; i < wordsByClauses.Count; i++)
+            {
+                var addedClause = new Clause(addedText, i.ToString(), wordsByClauses[i].clause);
+                var realizations = wordsByClauses[i].tokens;
+                for (int j = 0; j < realizations.Count; j++)
+                {
+                    var addedRealization = new Realization(addedClause, j.ToString(), realizations[j].lexemeOne, realizations[j].lexemeTwo);
+                    for (int k = 0; k < realizations[j].graphemes.Count; k++)
+                    {
+                        addedRealization.letters.Add(new Grapheme(addedRealization, k.ToString(), realizations[j].graphemes[k]));
+                    }
+                    addedClause.realizations.Add(addedRealization);
+                }
+                addedText.clauses.Add(addedClause);
+            }
+            analyzedDocument.texts.Add(addedText);
+            using (StreamWriter w = new StreamWriter(Path.Combine(_environment.ContentRootPath, "database", "documents", analyzedDocument.documentID + "_" + analyzedDocument.documentName + ".json")))
+            {
+                w.Write(analyzedDocument.Jsonize());
             }
             return RedirectToPage();
 
